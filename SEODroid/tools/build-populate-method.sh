4cd5b044595c05baf185237acbf8325d2bdef026
Matías Javier Rossi <matiasjrossi@gmail.com>
2011-11-13 21:57:58 -0300

    Ignore temporary files *~ Fixed populate method generator

--- SEODroid/tools/build-populate-method.sh
+++ SEODroid/tools/build-populate-method.sh
@@ -4,6 +4,7 @@ THISDIR=$( cd $(dirname $0) && pwd)
 DICTSOURCE='SEO-GoogleMaps-names.txt'
 
 echo 'private void populateGm2seoDict() {'
+echo -e "\tgm2seo = new Hashtable<String, SEOStreet>();"
 
 sed 's/^/\tgm2seo.put("/g' < $THISDIR/$DICTSOURCE | sed 's/|/", new SEOStreet("/g' | sed 's/:/", "/g' | sed 's/$/"));/g'
 
